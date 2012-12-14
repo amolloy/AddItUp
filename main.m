ad6dfdc764fabe68c7f52e7d6b450b93173eb62e
Andy Molloy <asmolloy@me.com>
2012-12-14 11:22:20 -0500

    Replace copyright boilerplate

--- main.m
+++ main.m
@@ -3,7 +3,7 @@
 //  iAdder
 //
 //  Created by Andrew Molloy on 6/10/09.
-//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//  Copyright Andy Molloy 2013. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
